42cb24de225bc54a797d0609bedcfba176d1482f
John Nguyen <33184644+johnnguyen3196@users.noreply.github.com>
2020-06-30 16:07:33 -0500

    Player movement and shooting
    User can drag plane around and shoot automatically.

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class playerMovement : MonoBehaviour
+{
+    private int nextUpdate = 1;
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextUpdate)
+        {
+            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
+            UpdateEverySecond();
+        }
+
+        if (Input.touchCount > 0)
+        {
+
+            Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
+
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                // get the touch position from the screen touch to world point
+                Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));
+                // lerp and set the position of the current object to that of the touch, but smoothly over time.
+                transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime);
+            }
+        }
+    }
+
+    void UpdateEverySecond()
+    {
+        Vector3 leftBulletPos = new Vector3(transform.position.x - 0.233f, transform.position.y + 0.371f, transform.position.z);
+        Vector3 rightBulletPos = new Vector3(transform.position.x + 0.233f, transform.position.y + 0.371f, transform.position.z);
+        GameObject go1 = Instantiate(bulletPrefab, leftBulletPos, Quaternion.identity);
+        GameObject go2 = Instantiate(bulletPrefab, rightBulletPos, Quaternion.identity);
+        Bullet bullet1 = go1.GetComponent<Bullet>();
+        Bullet bullet2 = go2.GetComponent<Bullet>();
+        bullet1.targetVector = new Vector3(0, 1, 0);
+        bullet2.targetVector = new Vector3(0, 1, 0);
+    }
+}
